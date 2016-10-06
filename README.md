8a5bbf5cd162c6863ec5740f614502d57b4404b5
chulanovskyi <skyrocker@mail.ru>
2016-10-07 00:17:17 +0300

    finally! will do refactoring after review...

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 10_coursera
+
+###Prerequisites:
+
+Run in console `pip install -r requirements.txt` to install 3rd party modules.
+
+---
+
+Script uses coursera xml feed to find some random courses and save info about them into Excel file.
+
+###How to use:
+
+You can edit in-script constants to get result as you want.
+
+| Constant | Description |
+| --- | --- |
+| `COURSE_COUNT` | how many random courses need to find |
+
+#!!!WARNING!!!
+
+This script is not 'code-support' friendly. I'm barely can remember how this works after a couple of days... But it works!
--- coursera.py
+++ coursera.py
@@ -2,11 +2,15 @@ import xml.etree.ElementTree as ET
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+from openpyxl.styles import Font
 import json
+import random
 
 
-COURSE_COUNT = 5
+COURSE_COUNT = 20
 CELLS_NAME = ('Name', 'Language', 'Start date', 'Duration', 'Rating')
+COL_WIDTH_L = 50
+COL_WIDTH_S = 25
 
 
 def get_courses_list():
@@ -29,14 +33,13 @@ def get_course_info(course_link):
     course_duration = basic_info.find(string='Commitment')
     if course_duration:
         course_duration = ' '.join(course_duration.next_element.stripped_strings)
-    course_start_date = soup.select('.rc-CourseGoogleSchemaMarkup')
+    course_start_date = soup.select('.rc-CourseGoogleSchemaMarkup') 
     if course_start_date:
         course_start_date = json.loads(course_start_date[0].text)['hasCourseInstance'][0]['startDate']
     course_rating = soup.select('.ratings-text.bt3-visible-xs')
     if course_rating:
         course_rating = course_rating[0].text.split(' ')[0]
     full_info = [course_name, course_lang, course_start_date, course_duration, course_rating]
-    print('Getting %s' % course_name)
     return full_info
 
 
@@ -46,18 +49,30 @@ def output_courses_info_to_xlsx(filepath, courses):
     ws.title = '20 random courses from Coursera'
     for name, cell in enumerate(list(ws['A1:E1'])[0]):
         cell.value = CELLS_NAME[name]
-    ###THIS PART NOT WORKING
-    for course in ws.iter_rows(min_row=2, max_col=5, max_row=COURSE_COUNT+1):
-        for cell_name, cell in enumerate(course):
-            if courses[cell_name]:
-                cell.value = courses[cell_name]
-    ###
+        cell.font = Font(bold=True)
+    ws.column_dimensions['A'].width = COL_WIDTH_L
+    ws.column_dimensions['B'].width = COL_WIDTH_L
+    ws.column_dimensions['C'].width = COL_WIDTH_S
+    ws.column_dimensions['D'].width = COL_WIDTH_S
+    ws.column_dimensions['E'].width = COL_WIDTH_S
+    for row in ws.iter_rows(min_row=2, max_col=5, max_row=COURSE_COUNT+1):
+        for cell_name, cell in enumerate(row):
+            if courses[cell.row-2][cell_name]:
+                cell.value = courses[cell.row-2][cell_name]
     wb.save(filepath)
 
 
 if __name__ == '__main__':
+    print('Getting course list...')
     courses_list = get_courses_list()
+    print('Done!')
     courses_info = []
+    print('Getting %d courses info...' % COURSE_COUNT)
     while len(courses_info) != COURSE_COUNT:
-        courses_info.append(get_course_info(courses_list.pop()))
+        course_info = get_course_info(courses_list.pop(courses_list.index(random.choice(courses_list))))
+        if course_info:
+            courses_info.append(course_info)
+    print('Done!')
+    print('Making excel file...')
     output_courses_info_to_xlsx('Coursera.xlsx', courses_info)
+    print('Finish!')
